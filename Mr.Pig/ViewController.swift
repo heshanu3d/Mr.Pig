415305580f7a102441a53f38992109c9eece02ed
heshanu3d <heshann@foxmail.com>
2019-01-06 12:15:35 +0800

    SceneKit 3D Games. Section IV
    1.incorporate SceneKit into existing applications
    
    2.SCNScene使用SpriteKit的SKTransition转换时候，由于场景切换会撕裂纹理，造成真机调试会报-[MTLDebugRenderCommandEncoder validateFramebufferWithRenderPipelineState:]:1196: failed assertion `For color attachment 0, the render pipeline's pixelFormat (MTLPixelFormatRGBA8Unorm_sRGB) does not match the framebuffer's pixelFormat (MTLPixelFormatBGR10_XR_sRGB).'
    的错误；使用真机离线运行，在场景切换的时候，SCNNode会渲染失败，但是能切换到下一场景。
    
    不知道更新到下一个xcode版本会不会fix这个bug。
    
    3.i won't finish the rest work of this game as everything left is easy for me, and i cannot learn a lot from it. so  that's all.

--- Mr.Pig/ViewController.swift
+++ Mr.Pig/ViewController.swift
@@ -1,25 +1,99 @@
-//
-//  ViewController.swift
-//  Mr.Pig
-//
-//  Created by hs on 2019/1/5.
-//  Copyright © 2019年 hs. All rights reserved.
-//
-
 import UIKit
-
+import SceneKit
+import SpriteKit
+// 2
 class ViewController: UIViewController {
-
+    // 3
+    let game = GameHelper.sharedInstance
+    var scnView: SCNView!
+    var gameScene: SCNScene!
+    var splashScene: SCNScene!
+    
+  var pigNode: SCNNode!
+  var cameraNode: SCNNode!
+  var cameraFollowNode: SCNNode!
+  var lightFollowNode: SCNNode!
+  var trafficNode: SCNNode!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 4
+        setupScenes()
+        setupNodes()
+        setupActions()
+        setupTraffic()
+        setupGestures()
+        setupSounds()
+        // 5
+        game.state = .tapToPlay
     }
-
+    func setupScenes() {
+        scnView = SCNView(frame: self.view.frame)
+        self.view.addSubview(scnView)
+        gameScene = SCNScene(named: "/MrPig.scnassets/GameScene.scn")
+        splashScene = SCNScene(named: "/MrPig.scnassets/SplashScene.scn")
+        scnView.scene = gameScene//splashScene
+        scnView.allowsCameraControl = true
+        mstartGame()
+    }
+  func setupNodes() {
+    pigNode = gameScene.rootNode.childNode(withName: "MrPig", recursively: true)!
+    cameraNode = gameScene.rootNode.childNode(withName: "camera", recursively: true)!
+    cameraNode.addChildNode(game.hudNode)
+    cameraFollowNode = gameScene.rootNode.childNode(withName: "FollowCamera", recursively: true)!
+    lightFollowNode = gameScene.rootNode.childNode(withName: "FollowLight", recursively: true)!
+    trafficNode = gameScene.rootNode.childNode(withName: "Traffic", recursively: true)!
+  }
+    func setupActions() {
+    }
+    func setupTraffic() {
+    }
+    func setupGestures() {
+    }
+    func setupSounds() {
+    }
+    
+    func mstartGame() {
+        splashScene.isPaused = true
+        self.game.state = .playing
+        self.setupSounds()
+        self.gameScene.isPaused = false
+    }
+    
+    func startGame() {
+        splashScene.isPaused = true
+        let transition = SKTransition.doorsOpenVertical(withDuration: 1.0)
+        scnView.present(gameScene, with: transition, incomingPointOfView: nil,
+                        completionHandler: {
+                            self.game.state = .playing
+                            self.setupSounds()
+                            self.gameScene.isPaused = false
+        })
+    }
+    
+    func stopGame() {
+        game.state = .gameOver
+        game.reset()
+    }
+    
+    func startSplash() {
+        gameScene.isPaused = true
+        let transition = SKTransition.doorsOpenVertical(withDuration: 1.0)
+        scnView.present(splashScene, with: transition, incomingPointOfView:
+            nil, completionHandler: {
+                self.game.state = .tapToPlay
+                self.setupSounds()
+                self.splashScene.isPaused = false
+        }) }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+//        if game.state == .tapToPlay {
+//            startGame()}
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    override var prefersStatusBarHidden : Bool { return true }
+    override var shouldAutorotate : Bool { return false }
 }
-
